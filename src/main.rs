4d2a98b0cc86f0ef157785f587f533e96ea2d189
Robert <johnidevo@gmail.com>
2022-07-03 13:57:18 +0000

    git commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
-
-
 
 fn main() {
-	
-	println!("{}", Path::new("../Artifacts/Addition.bin-runtime").exists());
-
 	let mut file = File::open("../Artifacts/Addition.bin-runtime").expect("File not found");
 	let mut data = String::new();
 	file.read_to_string(&mut data).expect("Error while reading file");
