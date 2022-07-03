f4d6370096eda3edf073b5c66b24dee98a3b101c
Robert <johnidevo@gmail.com>
2022-07-03 14:21:15 +0000

    git commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::num::ParseIntError;
+use std::error::Error;
+use std::path::Path;
 
 fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..(s.len()-1))
@@ -9,15 +11,25 @@ fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
         .collect()
 }
 
-fn main() {
+fn run() -> Result<(), Box<dyn Error>> {
+
+	println!("{}", Path::new("../Artifacts/Addition.bin-runtime").exists());
+	
 	let mut file = File::open("../Artifacts/Addition.bin-runtime").expect("File not found");
 	let mut buffer = String::new();
 	file.read_to_string(&mut buffer).expect("Error while reading file");
 	println!("{}", buffer);
+	
 	let bytes = decode(&buffer)?;
 
 	for b in &bytes {
 			println!("0x{:x}", b) 
 	}
 	println!("{}", buffer);
+	
+	Ok(())
+}
+
+fn main() {
+    run().unwrap();
 }
