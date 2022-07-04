32ae2960c4412b8a18cae439f829fb98e5e50922
Robert <johnidevo@gmail.com>
2022-07-04 11:38:29 +0000

    test commit terminal

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ enum Opcode {
     PUSH1(usize, u8), // 0x60
     PUSH2(usize, u8, u8), // 0x61
     //PUSH32(usize, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8), // 0x7f 
-
+	// test commit mesage terminal
     EOF,
 }
 
