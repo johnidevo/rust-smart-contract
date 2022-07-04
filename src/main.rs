1705c2f3bf522bfbedc742471211239f1907504f
Robert <johnidevo@gmail.com>
2022-07-04 11:25:11 +0000

    git commit

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
 impl Opcode {
     fn describe(&self) {
         match self {
-        Opcode::STOP(line) => println!("0x{:x}\tSTOP\tHalts execution", line),
-        Opcode::ADD(line) => println!("0x{:x}\tADD\tAddition operation", line),
-        Opcode::MUL(line) => println!("0x{:x}\tMUL\tMultiplication operation", line),
-        Opcode::PUSH1(line, x) => println!("0x{:x}\tPUSH1\tPlace 1-byte item on the stack 0x{:x}", line, x),
-        Opcode::PUSH2(line, x0, x1) => println!("0x{:x}\tPUSH2\tPlace 2-bytes item on the stack 0x{:x} 0x{:x}", line, x0, x1),
-        _ => println!("Unknown opcode")
+					Opcode::STOP(line) => println!("0x{:x}\tSTOP\tHalts execution", line),
+					Opcode::ADD(line) => println!("0x{:x}\tADD\tAddition operation", line),
+					Opcode::MUL(line) => println!("0x{:x}\tMUL\tMultiplication operation", line),
+					Opcode::PUSH1(line, x) => println!("0x{:x}\tPUSH1\tPlace 1-byte item on the stack 0x{:x}", line, x),
+					Opcode::PUSH2(line, x0, x1) => println!("0x{:x}\tPUSH2\tPlace 2-bytes item on the stack 0x{:x} 0x{:x}", line, x0, x1),
+					_ => println!("Unknown opcode")
  
         }
     }
