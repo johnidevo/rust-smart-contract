6d9aa3fc8dc2de9d68031c3d86543442e470a800
Robert <johnidevo@gmail.com>
2022-07-07 12:45:00 +0000

    commit

--- .assets/git.sh
+++ .assets/git.sh
@@ -5,5 +5,5 @@ read -p "Enter your commit message: " varname
 varname=${varname:-commit}
 
 git commit -m "$varname" #"git commit"
-git remote add origin https://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@github.com/johnidevo/rust-smart-contract.git
+git remote add origin https://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@github.com/johnidevo/rust-smart-contract.git
 git push origin master
